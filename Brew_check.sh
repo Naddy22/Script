7a3df4827f724737d093832a47295bf0af3709a2
Nadege Moisan <namoisan@c1r3p7.42quebec.com>
2024-09-05 11:14:10 -0400

    mise à la norme de bash

--- Brew_check.sh
+++ Brew_check.sh
@@ -1,47 +1,62 @@
 #!/bin/bash
 
-echo running...
+# -- COLORS -- #
+BLACK=\\033[30m # Black
+RED=\\033[31m # Red
+GREEN=\\033[32m # GREEN
+YELLOW=\\033[33m # YELLOW
+BLUE=\\033[34m # BLUE
+PURPLE=\\033[35m # PURPLE
+CYAN=\\033[36m # CYAN
+WHITE=\\033[0m # WHITE
+ITALIC=\\033[3m
 
-@if [ -x "$$HOME/homebrew/bin/brew" ] || [ -x "$$HOME/.brew/bin/brew" ]; then \
-	echo "$(GREEN)✔︎ $(ITALIC)Brew is already installed$(RESET)$(GREEN) ✔︎$(RESET)"; \
-else \
-	echo "$(RED)✗ $(ITALIC)Brew not found$(RESET)$(RED) ✗"; \
-	read -p "Do you want to install brew? y/n: "  brewchoice; \
-	printf "$(RESET)"; \
-	if [ "$$brewchoice" = "y" ]; then \
-		rm -rf $$HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $$HOME/.brew && \
-		echo 'export PATH=$$HOME/.brew/bin:$$PATH' >> $$HOME/.zshrc && source $$HOME/.zshrc && brew update; \
-		echo "$(GREEN)✔︎ $(ITALIC)Brew successfully installed$(RESET)$(GREEN) ✔︎$(RESET)"; \
-	else \
-		echo "Exit"; \
-		exit 2; \
-	fi \
+if [ -x "$HOME/homebrew/bin/brew" ] || [ -x "$HOME/.brew/bin/brew" ];
+then
+	printf "$GREEN ✔︎$ITALIC Brew is already installed$WHITE$GREEN ✔︎$WHITE\n";
+else
+	printf "$RED ✗$ITALIC Brew not found$WHITE$RED ✗\n"; 
+	read -p "Do you want to install brew? y/n: "  brewchoice;
+	printf "$WHITE\n";
+	if [ "$brewchoice" = "y" ];
+	then
+		rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew &&
+		printf 'export PATH=$$HOME/.brew/bin:$$PATH' >> $HOME/.zshrc && source $HOME/.zshrc && brew update;
+		printf "$GREEN ✔︎$ITALIC Brew successfully installed$WHITE$GREEN ✔︎$WHITE\n";
+	else
+		printf "Exit";
+		exit 2;
+	fi
 fi
-@if [ -d "$$HOME/homebrew/opt/cmake/bin" ] || [ -d "$$HOME/.brew/opt/cmake/bin" ]; then \
-	echo "$(GREEN)✔︎ $(ITALIC)cmake is already installed$(RESET)$(GREEN) ✔︎$(RESET)"; \
-else \
-	echo "$(RED)✗ $(ITALIC)cmake not found$(RESET)$(RED) ✗"; \
-	read -p "Do you want to install cmake? y/n: " cmakechoice; \
-	printf "$(RESET)"; \
-	if [ "$$cmakechoice" = "y" ]; then \
-		brew install cmake; \
-		echo "$(GREEN)✔︎ $(ITALIC)cmake successfully installed$(RESET)$(GREEN) ✔︎$(RESET)"; \
-	else \
-		echo "Exit"; \
-		exit 2; \
-	fi \
+if [ -d "$HOME/homebrew/opt/cmake/bin" ] || [ -d "$HOME/.brew/opt/cmake/bin" ];
+then
+	printf "$GREEN ✔︎$ITALIC cmake is already installed$WHITE$GREEN ✔︎$WHITE\n";
+else
+	printf "$RED ✗$ITALIC cmake not found$WHITE$RED ✗\n";
+	read -p "Do you want to install cmake? y/n: " cmakechoice;
+	printf "$WHITE\n";
+	if [ "$cmakechoice" = "y" ];
+	then
+		brew install cmake;
+		printf "$GREEN ✔︎$ITALIC cmake successfully installed$WHITE$GREEN ✔︎$WHITE\n";
+	else
+		printf "Exit";
+		exit 2;
+	fi
 fi
-@if [ -d "$$HOME/homebrew/opt/glfw/lib" ] || [ -d "$$HOME/.brew/opt/glfw/lib" ]; then \
-	echo "$(GREEN)✔︎ $(ITALIC)glfw is already installed$(RESET)$(GREEN) ✔︎$(RESET)"; \
-else \
-	echo "$(RED)✗ $(ITALIC)glfw not found$(RESET)$(RED) ✗"; \
-	read -p "Do you want to install glfw? y/n: " glfwchoice; \
-	printf "$(RESET)"; \
-	if [ "$$glfwchoice" = "y" ]; then \
-		brew install glfw; \
-		echo "$(GREEN)✔︎ $(ITALIC)glfw successfully installed$(RESET)$(GREEN) ✔︎$(RESET)"; \
-	else \
-		echo "Exit"; \
-		exit 2; \
-	fi \
+if [ -d "$HOME/homebrew/opt/glfw/lib" ] || [ -d "$HOME/.brew/opt/glfw/lib" ];
+then
+	printf "$GREEN ✔︎$ITALIC glfw is already installed$WHITE$GREEN ✔︎$WHITE\n";
+else
+	printf "$RED ✗$ITALIC glfw not found$WHITE$RED ✗\n";
+	read -p "Do you want to install glfw? y/n: " glfwchoice;
+	printf "$WHITE\n";
+	if [ "$glfwchoice" = "y" ];
+	then
+		brew install glfw;
+		printf "$GREEN ✔︎$ITALIC glfw successfully installed$WHITE$GREEN ✔︎$WHITE\n";
+	else
+		printf "Exit";
+		exit 2;
+	fi
 fi
